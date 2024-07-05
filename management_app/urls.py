664b426505196d819f83e63b150635c2b929b4f7
SaabaV <deutsch.vlad223@gmail.com>
2024-07-05 10:52:43 +0200

    Add homework tastks

--- management_app/urls.py
+++ management_app/urls.py
@@ -4,11 +4,17 @@ from management_app.views import (
     get_all_projects,
     get_all_tags,
     get_tag_by_id,
-    create_new_tag
+    create_new_tag,
+    TaskCreateView,
+    TaskListView,
+    TaskStatsView
 )
 
 urlpatterns = [
     path('projects/', get_all_projects),
+    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
     path('tags/', get_all_tags),
     path('tags/<int:pk>/', get_tag_by_id),
     path('tags/create/', create_new_tag)
--- management_app/views.py
+++ management_app/views.py
@@ -2,10 +2,19 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Count, Q
+import datetime
 
 from management_app.models import Project, Tag
 from management_app.serializers.projects import AllProjectsSerializer
 from management_app.serializers.tags import AllTagsSerializer
+from .models import Task
+from serializers.tasks import AllTasksSerializer
 
 
 @api_view(["GET"])
@@ -50,3 +59,31 @@ def get_tag_by_id(request: Request, pk: int) -> Response:
 
     serialize = AllTagsSerializer(tag_by_id)
     return Response(data=serialize.data, status=status.HTTP_200_OK)
+
+
+class TaskCreateView(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = AllTasksSerializer
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = AllTasksSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['status', 'deadline']
+    ordering_fields = ['deadline']
+    ordering = ['deadline']
+
+
+class TaskStatsView(APIView):
+    def get(self, request, *args, **kwargs):
+        total_tasks = Task.objects.count()
+        status_counts = Task.objects.values('status').annotate(count=Count('status'))
+        overdue_tasks = Task.objects.filter(deadline__lt=datetime.now(), status__ne='Done').count()
+
+        stats = {
+            'total_tasks': total_tasks,
+            'status_counts': status_counts,
+            'overdue_tasks': overdue_tasks,
+        }
+        return Response(stats)
