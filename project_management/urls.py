2d5b135a1978925e5e6cd2ced20e408ad718a927
SaabaV <deutsch.vlad223@gmail.com>
2024-07-17 19:28:12 +0200

    Added JWT authentication and permissions, integrated Swagger for API documentation

--- project_management/urls.py
+++ project_management/urls.py
@@ -36,16 +36,18 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('management_app.urls')),
+    path('api/v1/', include(router.urls)),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
 
 
